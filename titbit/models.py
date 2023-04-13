005e610abbd73ecc22b894217617c9026956faa8
luandretta <>
2023-04-13 13:05:56 +0000

    Feat: create profile when new user signs up

--- titbit/models.py
+++ titbit/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 """
@@ -7,11 +9,25 @@ Create a User Profile Model
 """
 class Profile(models.Model):
     user = models.OneToOneField(User,
-        related_name="profile", on_delete=models.CASCADE)
+                                related_name="profile", 
+                                on_delete=models.CASCADE)
     follows = models.ManyToManyField('self',
-        related_name='followed_by',
-        symmetrical=False,
-        blank=True)
+                                    related_name='followed_by',
+                                    symmetrical=False,
+                                    blank=True)
 
     def __str__(self):
         return self.user.username
+
+"""
+Create Profile when new User signs up
+"""
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        # Have the user follow themselves
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
