0467b7f3f62360c3e64f162e3e9c3fbd7b401910
luandretta <>
2023-04-13 12:43:52 +0000

    Feat: create Profile Model

--- titbit/admin.py
+++ titbit/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
-
+from .models import Profile
 
 # Unregister Groups
 admin.site.unregister(Group)
 
+
+# Mix Profile info into User info
+class ProfileInline(admin.StackedInline):
+    model = Profile
 
 
 # Extend User Model
@@ -12,8 +16,12 @@ class UserAdmin(admin.ModelAdmin):
     model = User
     # Just display username fields on admin page
     fields = ['username']
+    inlines = [ProfileInline]
+
 
 # Unregister initial User
 admin.site.unregister(User)
-# Register User
+
+# Register User and Profile
 admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
--- titbit/models.py
+++ titbit/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+"""
+Create a User Profile Model
+"""
+class Profile(models.Model):
+    user = models.OneToOneField(User,
+        related_name="profile", on_delete=models.CASCADE)
+    follows = models.ManyToManyField('self',
+        related_name='followed_by',
+        symmetrical=False,
+        blank=True)
+
+    def __str__(self):
+        return self.user.username
