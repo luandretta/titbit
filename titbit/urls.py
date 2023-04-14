bfb762e90d854f7b081f2ea6af8ad0c15161eaf2
Lucimeri Andretta <luandretta@hotmail.com>
2023-04-14 08:53:17 +0000

    Feat: edit profile.html

--- titbit/urls.py
+++ titbit/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('profile_list', views.profile_list, name='profile_list' )
+    path('profile_list', views.profile_list, name='profile_list'),
+    path('profile/<int:pk>', views.profile, name='profile'),
 ]
--- titbit/views.py
+++ titbit/views.py
@@ -14,3 +14,11 @@ def profile_list(request):
         messages.success(request, ('You must be loggeg in to view this page.'))
         return redirect('home')
 
+
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+        return render(request, 'profile.html', {'profile':profile})
+    else:
+        messages.success(request, ('You must be loggeg in to view this page.'))
+        return redirect('home')
