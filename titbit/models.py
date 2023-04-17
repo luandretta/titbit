9c92614c08fd57af87f69e7acc9ae18f9cd415c6
Lucimeri Andretta <luandretta@hotmail.com>
2023-04-17 06:48:01 +0000

    Feat(models): create post model

--- titbit/models.py
+++ titbit/models.py
@@ -5,13 +5,31 @@ from django.dispatch import receiver
 
 
 """
+Create Post Model with date
+"""
+class Post(models.Model):
+    user = models.ForeignKey(
+        User, related_name='posts',
+        on_delete=models.CASCADE)
+    body = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f'{self.user} '
+            f'({self.created_at:%d-%m-%Y %H:M}): '
+            f'{self.body}...'
+        )
+
+
+"""
 Create a User Profile Model
 List follows and followed by
 Insert date to last update
 """
 class Profile(models.Model):
     user = models.OneToOneField(User,
-                                related_name="profile", 
+                                related_name="profile",
                                 on_delete=models.CASCADE)
     follows = models.ManyToManyField('self',
                                     related_name='followed_by',
@@ -35,4 +53,3 @@ def create_profile(sender, instance, created, **kwargs):
         # Have the user follow themselves
         user_profile.follows.set([instance.profile.id])
         user_profile.save()
-
